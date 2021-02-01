e14b8a91022cb8d21bef9031f573ac0204c3e0bb
AlexandrTug <50037169+AlexandrTug@users.noreply.github.com>
2021-02-01 23:09:49 +0300

    Add files via upload
    Edited to confirm PEP8

--- http_server.py
+++ http_server.py
@@ -1,39 +1,45 @@
-import requests
+
 import http.server
 import socketserver
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+import requests
+
+
 class HttpProcessor(BaseHTTPRequestHandler):
 
-    habr_url='https://habr.com'
-    tag_open='<'
-    tag_close='>'
+    habr_url = 'https://habr.com'
+    tag_open = '<'
+    tag_close = '>'
     in_tag = False
     is_space = False
-    is_char =False
+    is_char = False
     char_count = 0
-    space_set={ ' ', ',', '.', '!', ':', ';', '?'}
+    space_set={' ', ',', '.', '!', ':', ';', '?', '\n', '\r'}
 
     def ins_TM(self, in_text):
+        
         out_text=''
         for i in range(len(in_text)):
 
-            if in_text[i] == self.tag_open : 
+            if in_text[i] == self.tag_open :
+                if self.char_count == 6:
+                    out_text += u"\u2122" 
                 self.in_tag = True
                 self.char_count =0
 
             elif in_text[i] == self.tag_close : 
                 self.in_tag = False
 
-            elif ( not(self.in_tag) and ( in_text[i] in self.space_set ) ) :
+            elif (not(self.in_tag) and (in_text[i] in self.space_set)) :
                 self.is_space = True
                 if self.char_count == 6:
-                    out_text +=u"\u2122"
-                self.char_count=0
+                    out_text += u"\u2122"
+                self.char_count = 0
 
             elif not(self.in_tag):
                 self.is_char = True
-                self.char_count +=1
+                self.char_count += 1
             
             out_text+=in_text[i]    
 
@@ -41,24 +47,20 @@ class HttpProcessor(BaseHTTPRequestHandler):
 
     def do_GET(self):
 
-        print('receive command:',self.command)
-        print('path:',self.path)
-        print('requestline:',self.requestline)
+        print('receive command:', self.command)
+        print('path:', self.path)
+        print('requestline:', self.requestline)
 
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         
         r=requests.get(self.habr_url+self.path)
-        localhost_text=r.text.replace(self.habr_url, 'http://localhost:'+str(PORT))
-        rep_text=self.ins_TM(localhost_text)
+        localhost_text = r.text.replace(self.habr_url, 'http://localhost:'+str(PORT))
+        rep_text = self.ins_TM(localhost_text)
         self.wfile.write(rep_text.encode('utf-8'))
-        
+    
  
- 
-
-        
-        
 PORT=8080
 
 
